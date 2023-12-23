1c9da5d6e01bdb4a56bd608b06c7fd85ceb56f5a
Geared <114871309+Sonic-Geared@users.noreply.github.com>
2023-12-23 15:46:02 -0300

    10 didn't work so let's try -1!
    uhhhhhh yeah

--- RSDKv4/RetroEngine.hpp
+++ RSDKv4/RetroEngine.hpp
@@ -243,6 +243,8 @@ enum RetroEngineMessages {
 };
 
 enum RetroStates {
+    // There's a reason of why we're adding a 2nd version of it. Do not touch it.
+    ENGINE_VIDEOWAIT2  = -1,
     ENGINE_DEVMENU     = 0,
     ENGINE_MAINGAME    = 1,
     ENGINE_INITDEVMENU = 2,
@@ -253,8 +255,6 @@ enum RetroStates {
     ENGINE_ENDGAME     = 7,
     ENGINE_RESETGAME   = 8,
     ENGINE_VIDEOWAIT   = 9,
-    // There's a reason of why we're adding a 2nd version of it. Do not touch it.
-    ENGINE_VIDEOWAIT2   = 10,
 
 #if !RETRO_USE_ORIGINAL_CODE && RETRO_USE_NETWORKING
     // Custom GameModes (required to make some features work)
